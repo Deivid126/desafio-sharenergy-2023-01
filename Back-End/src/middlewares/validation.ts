51cd8687a49bc7a687cf30de887df984b9bd5e6b
Deivid Gonçalves <deividg2022@outlook.com>
2022-12-29 16:03:28 -0300

    validation of post

---
+++ Back-End/src/middlewares/validation.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+
+export default function validantion(res:Response,req:Request, netx:NextFunction){
+
+    const {nome, email,telefone,endereco,senha,cpf } = req.body;
+
+    if(!nome){
+        return res.status(400).json({msg:"Nome é obrigatório"})
+    }
+    if(!email){
+        return res.status(400).json({msg:"E-mail é obrigatório"})
+    }
+    if(!telefone){
+        return res.status(400).json({msg:"Telefone é obrigatório"})
+    }
+    if(!endereco){
+        return res.status(400).json({msg:"Endereço é obrigatório"})
+    }
+    if(!senha){
+        return res.status(400).json({msg:"Senha é obrigatório"})
+    }
+    if(!cpf){
+        return res.status(400).json({msg:"Cpf é obrigatório"})
+    }
+
+    return netx();
+
+}
--- Back-End/src/routes/index.ts
+++ Back-End/src/routes/index.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import Controller from "../controller/User"
 import Login from "../controller/Login"
 import authenticaded from "../middlewares/auth";
+import validantion from "../middlewares/validation";
 
 const routes = Router();
 
-routes.post("/user",authenticaded,Controller.create);
+routes.post("/user",authenticaded,validantion,Controller.create);
 routes.get("/user",authenticaded,Controller.findAll);
 routes.get("/user/:id",authenticaded,Controller.findOne);
 routes.put("/user/:id",authenticaded, Controller.uptade);
